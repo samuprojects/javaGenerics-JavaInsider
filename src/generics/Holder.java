c865d9b1789e961640ee00cf160f28b38369940d
Samuel <samugit.projects@gmail.com>
2024-01-05 15:19:55 -0300

    Aula01 Um exemplo de código sem o uso de generics

---
+++ src/generics/Holder.java
@@ -0,0 +1,15 @@
+package generics;
+
+@SuppressWarnings("ALL")
+public class Holder {
+
+    private Object obj;
+
+    public Holder(Object obj) {
+        this.obj = obj;
+    }
+
+    public Object getObj() {
+        return obj;
+    }
+}
---
+++ src/theory/Concepts.java
@@ -0,0 +1,13 @@
+package theory;
+
+@SuppressWarnings("ALL")
+public class Concepts {
+
+    /*
+    *
+    * Generics foi introduzido no Java 5 responsável por grandes mudanças
+    *
+    * Por exemplo, na Classe Holder temos uma classe que instancia um Object e recebe um Object como parâmetro e retorna esse Object
+    * Para utilizar isso deveríamos fazer um casting desse Object para o formato esperado, string, Integer, etc). (Sem uso de Generics)
+    * */
+}
